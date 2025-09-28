285fb40466f25fcbec5b859058619b36a624d73a
estew <masaki.eric@me.com>
2025-09-28 01:34:59 -0400

    initial try at unsafe smtp

---
+++ ansible/roles/linux_deploy_smtp/files/unsafe_mail_handler.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# unsafe_mail_handler.py
+# WARNING: lets wooden horses in
+import sys
+import os
+import email
+import pathlib
+
+def main():
+    # Postfix invokes mailbox_command with input on stdin (the message)
+    data = sys.stdin.buffer.read()
+    try:
+        msg = email.message_from_bytes(data)
+        raw_to = msg.get('To', 'unknown')
+    except Exception:
+        raw_to = 'unknown'
+    filename = raw_to.replace('@', '_at_').strip()
+    outdir = '/var/vuln_mailstore'
+    os.makedirs(outdir, exist_ok=True)
+    path = os.path.join(outdir, filename)\
+    with open(path, 'ab') as f:
+        f.write(b"---NEW MESSAGE---\n")
+        f.write(data if isinstance(data, bytes) else data.encode('utf-8', errors='ignore'))
+        f.write(b"\n---END MESSAGE---\n")
+    # return success
+    return
+
+if __name__ == '__main__':
+    main()
