816804a591cc0bb5dff2b9d7ad1313352fc2fb73
estew <masaki.eric@me.com>
2025-09-28 20:45:59 -0400

    edited binary

--- ansible/roles/linux_deploy_ssh/files/vuln_suid_binary.c
+++ ansible/roles/linux_deploy_ssh/files/vuln_suid_binary.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// MTAwIG1lYXN1cmVzIG9mIG9pbA==
 int main(int argc, char *argv[]) {
-    char cmd[100] = "/bin/cat ";
+    char barrelsofoil[100] = "/bin/cat ";
     if (argc < 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
-    strncat(cmd, argv[1], 90);
-    system(cmd);
+    strncat(barrelsofoil, argv[1], 90);
+    system(barrelsofoil);
     return 0;
 }
