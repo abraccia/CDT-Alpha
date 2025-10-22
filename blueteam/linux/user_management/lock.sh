0ba91c7cce31678a7b2d1d980a0c1a8e30b4a53e
Masaki Tanaka <masaki.eric@me.com>
2025-10-22 16:15:29 +0100

    adding greyteam users

--- blueteam/linux/user_management/lock.sh
+++ blueteam/linux/user_management/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Allowed usernames CHECK WHAT USERS EXIST FIRST!!
-allowed_users=("blueteam" "root" "greyteam")
+allowed_users=("blueteam" "root" "greyteam" "ansible" "greyteam2")
 
 # Get all local users with UID ≥ 1000 (typically real people, not system users)
 users_to_check=$(awk -F: '$3 >= 1000 { print $1 }' /etc/passwd)
